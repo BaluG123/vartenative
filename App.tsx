e1ce1424574423624f5e87197d074114f4f7e542
BaluG123 <balu1072000@gmail.com>
2023-05-02 22:22:15 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React,{useEffect} from "react";
+import { View } from "react-native/types";
+
+const App = () =>{
+
+  useEffect
+
+  return(
+     <View>
+      News App Checking
+     </View>
+  )
+}
+
+export default App;
